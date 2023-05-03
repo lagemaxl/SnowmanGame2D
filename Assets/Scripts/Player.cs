f483ec55037bcd7ef5c28788cabf505437b3e5f0
lagemaxl <85054751+lagemaxl@users.noreply.github.com>
2023-05-03 21:53:42 +0200

    player funguje jak má (dejme tomu)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,42 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // https://github.com/herbou/Tuto_DrawTrajectory/blob/master/Assets/Scripts/
-    // mozna bude potreba public
+    public float forceMultiplier = 10f;
+    private Vector2 startMousePosition;
+    private Vector2 endMousePosition;
     private Rigidbody2D rb;
-    private CircleCollider2D col;
 
-    public Vector3 Pos { get { return transform.position; } }
-
-    void Awake()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<CircleCollider2D>();
     }
 
-    public void Push(Vector2 force)
+    void Update()
     {
-        rb.AddForce(force, ForceMode2D.Impulse);
-    }
+        if (Input.GetMouseButtonDown(0))
+        {
+            startMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
 
-    public void ActivateRb()
-    {
-        rb.isKinematic = false;
-    }
-
-    public void DeactivateRb()
-    {
-        rb.velocity = Vector3.zero;
-        rb.angularVelocity = 0f;
-        rb.isKinematic = true;
-    }
-
-    // dodelat
-    public void KillPlayer()
-    {
-        Debug.Log("Umrer");
-        // Ukonèení hry
-        Application.Quit();
+        if (Input.GetMouseButtonUp(0))
+        {
+            endMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = startMousePosition - endMousePosition;
+            direction.Normalize();
+            rb.AddForce(direction * forceMultiplier, ForceMode2D.Impulse);
+        }
     }
 }
